2549372e995ba30bac7a300dc2cb3d58e590aad4
Swikriti Neupane <okayswik@gmail.com>
2022-06-19 19:28:09 +0545

    Adding objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+let logEntries = [];
+
 //Getting Users Input for each function
 function getUserInput() {
   return parseInt(userInput.value);
@@ -11,30 +13,49 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); //from vendor file
 }
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
 
+  console.log(logEntries);
+}
 function add() {
   const enteredNum = getUserInput();
   const initialResult = currentResult;
   currentResult += enteredNum;
   createAndWriteOutput("+", initialResult, enteredNum);
+  writeToLog("Add", initialResult, enteredNum, currentResult);
 }
 function subtract() {
   const enteredNum = getUserInput();
   const initialResult = currentResult;
   currentResult -= enteredNum;
   createAndWriteOutput("-", initialResult, enteredNum);
+  writeToLog("SUBTRACT", initialResult, enteredNum, currentResult);
 }
 function multiply() {
   const enteredNum = getUserInput();
   const initialResult = currentResult;
   currentResult = currentResult * enteredNum;
   createAndWriteOutput("*", initialResult, enteredNum);
+  writeToLog("MULTIPLY", initialResult, enteredNum, currentResult);
 }
 function divide() {
   const enteredNum = getUserInput();
   const initialResult = currentResult;
   currentResult = currentResult / enteredNum;
   createAndWriteOutput("/", initialResult, enteredNum);
+  writeToLog("DIVIDE", initialResult, enteredNum, currentResult);
 }
 
 addBtn.addEventListener("click", add);
