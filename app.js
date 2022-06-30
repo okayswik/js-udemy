37b5f96feb2bcee70717129501525d166bfb935b
Swikriti Neupane <okayswik@gmail.com>
2022-06-30 15:26:19 +0545

    Changed the log

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const http = require("http");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((request, response) => {
-  let body = [];
-  request.on("data", (chunk) => {
-    body.push(chunk);
-  });
-  request.on("end", () => {
-    body = Buffer.concat(body).toString();
-    let userName = "Unknown User";
-    if (body) {
-      userName = body.split("=")[1];
-    }
-    response.setHeader("Content-Type", "text/html");
-    response.write(
-      `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
-    );
-    response.end();
-  });
+const locationRoutes = require('./routes/location');
+
+const app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
-server.listen(3000);
+app.use(locationRoutes);
+
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   const userName = req.body.username || 'Unknown User';
+//   res.render('index', {
+//     user: userName
+//   });
+// });
+
+app.listen(3000);
